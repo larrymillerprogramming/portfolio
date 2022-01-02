6dd99b03427e722bb76f7a2b0dae376d90285a3e
Larry Miller <musicmrmiller@gmail.com>
2022-01-02 14:49:33 -0500

    update away from one-page model

--- index.js
+++ index.js
@@ -1,82 +1,27 @@
-// This function switches the content being viewed.
-function switchContent(div) {
-    let selectedDiv = div
-    // Run the hide animation
-    document.querySelector('.contentbox').style.animation = 'hide 0.5s forwards';
-
-    // After hide animation, change content and run show animation
-    setTimeout(function(){
-        document.querySelector('#welcome').style.display = 'none';
-        document.querySelector('#about').style.display = 'none';
-        document.querySelector('#projects').style.display = 'none';
-        document.querySelector('#resume').style.display = 'none';
-        document.querySelector('#data').style.display = 'none';
-        document.querySelector(selectedDiv).style.display = 'block';
-        document.querySelector('.contentbox').style.animation = 'show 0.5s forwards';
-        console.log(selectedDiv);
-        // Reset functionality after show animation
-        setTimeout(function(){
-            document.querySelector('.contentbox').style.animation = 'none';
-        }, 500)
-    }, 500)
-}
+let cssfile = 'style.css'
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Call switchContent with nav buttons
-    document.querySelector('#navabout').addEventListener('click', () => {
-        switchContent('#about');
-    });
-    document.querySelector('#navprojects').addEventListener('click', () => {
-        switchContent('#projects');
-    });
-    document.querySelector('#navresume').addEventListener('click', () => {
-        switchContent('#resume');
-    });
-    document.querySelector('#navdata').addEventListener('click', () => {
-        switchContent('#data');
-    });
+   // Detect which CSS file is active, apply it, and prepare the switch button
+   if (sessionStorage.getItem('night') != null && sessionStorage.getItem('night') == 'on') {
+      cssfile = 'style2.css';
+      document.querySelector('#cssswitch').innerHTML = '<i class="bi bi-brightness-high"></i>';
+   }
+   document.querySelector('#cssfile').href = cssfile;
 
-    // // Call switchContent with landing page buttons
-    // document.querySelector('#landabout').addEventListener('click', () => {
-    //     switchContent('#about');
-    // });
-    // document.querySelector('#landprojects').addEventListener('click', () => {
-    //     switchContent('#projects');
-    // });
-    // document.querySelector('#landresume').addEventListener('click', () => {
-    //     switchContent('#resume');
-    // });
-    // document.querySelector('#landdata').addEventListener('click', () => {
-    //     switchContent('#data');
-    // });
-
-    // Data link in Welcome page
-    document.querySelector('#datalink1').addEventListener('click', () => {
-        switchContent('#data');
-    });
-
-    // Data link in About page
-    document.querySelector('#datalink2').addEventListener('click', () => {
-        switchContent('#data');
-    });
-
-    // Show landing page when banner is clicked
-    document.querySelector('#bannerlink').addEventListener('click', () => {
-        switchContent('#welcome');
-    });
-
-    // Switch CSS files with darkmode switch
-    document.querySelector('#cssswitch').addEventListener('click', () => {
-         if (document.querySelector('#cssswitch').innerHTML == '<i class="bi bi-moon"></i>') {
-            document.querySelector('#cssswitch').innerHTML = '<i class="bi bi-brightness-high"></i>';
-            document.querySelector('#cssfile').href = 'style2.css';
-         }
-         else {
-            document.querySelector('#cssswitch').innerHTML = '<i class="bi bi-moon"></i>';
-            document.querySelector('#cssfile').href = 'style.css';
-         }
-         
-    });
+   // Switch CSS files with darkmode switch
+   document.querySelector('#cssswitch').addEventListener('click', () => {
+      if (document.querySelector('#cssswitch').innerHTML == '<i class="bi bi-moon"></i>') {
+         document.querySelector('#cssswitch').innerHTML = '<i class="bi bi-brightness-high"></i>';
+         document.querySelector('#cssfile').href = 'style2.css';
+         sessionStorage.setItem('night', 'on');
+      }
+      else {
+         document.querySelector('#cssswitch').innerHTML = '<i class="bi bi-moon"></i>';
+         document.querySelector('#cssfile').href = 'style.css';
+         cssfile = 'style.css';
+         sessionStorage.setItem('night', 'off');
+      }     
+   });
 
 })
